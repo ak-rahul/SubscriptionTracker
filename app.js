906a77aae3d85fd0bc45976b55be17205002751e
AK Rahul <rahulanilkumar2023@gmail.com>
2025-02-16 19:03:32 +0530

    Created a simple framwork

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("server running")
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("welcome to the Subscription tacker API !!");
+});
+
+app.listen(3000, () => {
+    console.log("Subscription Tracker APi running on port 3000"); 
+});
+
+export default app;
