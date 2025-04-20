d326d46ea330d82860d4cb6859dff9d4b9585870
AK Rahul <rahulanilkumar2023@gmail.com>
2025-04-20 21:06:37 +0530

    Successfully completed one controller

---
+++ controllers/auth.controller.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import { JWT_SECRET, JWT_EXPIRES_IN } from "../config/env.js";
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+export const signUp = async (req, res, next) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+        const { name , email, password } = req.body;
+
+        const existingUser = await User.findOne({email});
+        if(existingUser) {
+            const error = new Error("User alreay exists...");
+            error.statusCode = 409;
+            throw error;
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newUser = await User.create([{ name, email, password: hashedPassword }], { session });
+
+        const token = jwt.sign({userId: newUser[0]._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES_IN});
+
+        await session.commitTransaction();
+        session.endSession();
+
+        res.status(201).json({
+            success: true,
+            message: "User created successfully...",
+            data: {
+                token,
+                user: newUser[0]
+            }
+        })
+    } catch(error) {
+        await session.abortTransaction();
+        session.endSession();
+        next(error);
+    }
+}
+
+export const signIn = async (req, res, next) => {
+
+}
+
+export const signOut = async (req, res, next) => {
+
+}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,17 +1,10 @@
 import { Router } from "express";
+import { signIn, signUp, signOut } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-authRouter.post('sign-up' , (req, res) => {
-    res.send("Sign Up now........");
-})
-
-authRouter.post('sign-in' , (req, res) => {
-    res.send("Sign In now........");
-})
-
-authRouter.post('sign-out' , (req, res) => {
-    res.send("Byee........");
-})
+authRouter.post('/sign-up' , signUp);
+authRouter.post('/sign-in' , signIn);
+authRouter.post('/sign-out' , signOut);
 
 export default authRouter;
