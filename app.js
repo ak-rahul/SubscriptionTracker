53c16fea953ba99fbbba7703b60709f7a4af5433
AK Rahul <rahulanilkumar2023@gmail.com>
2025-04-20 12:06:04 +0530

    Database setup successfully

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import {PORT} from "./config/env.js"
 import authRouter from "./routes/auth.routes.js"
 import userRouter from "./routes/user.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -15,9 +16,11 @@ app.get('/' , (req, res) => {
     res.send("Welcome to Subscription Tracker API");
 })
 
-app.listen(PORT , () => {
+app.listen(PORT , async () => {
     console.log(`Server running on port ${PORT}`);
-    console.log(`Link : http://localhost:${PORT}`)
+    console.log(`Link : http://localhost:${PORT}`);
+
+    await connectToDatabase(); 
 })
 
 export default app;
--- database/mongodb.js
+++ database/mongodb.js
@@ -1,7 +1,18 @@
-import mongoose from mongoose
-import {DB_URI, PORT} from '../config/env.js'
+import mongoose from "mongoose";
+import {DB_URI, NODE_ENV} from '../config/env.js'
 
 if(!DB_URI) {
     throw new Error("Plase define MONGODB_URI in your .env.<production/>development>.local file......");
-    
 }
+
+const connectToDatabase = async() => {
+    try {
+        await mongoose.connect(DB_URI);
+        console.log(`Connected to database in ${NODE_ENV} mode..`);
+    } catch (error) {
+        console.error("Error connecting to database : ", error);
+        process.exit(1)  
+    }
+}
+
+export default connectToDatabase;
